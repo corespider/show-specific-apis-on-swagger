7f0b2caf6100be6cf28049be5f5887996b2f6394
Your Name <53593343+CoreProgramm@users.noreply.github.com>
2021-08-24 00:49:49 +0530

    Add project files.

---
+++ Core.SwaggerAPI/Filters/CustomSwaggerFilter.cs
@@ -0,0 +1,19 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+namespace Core.SwaggerAPI.Filters
+{
+    public class CustomSwaggerFilter : IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            var nonMobileRoutes = swaggerDoc.Paths
+                .Where(x => !x.Key.ToLower().Contains("public"))
+                .ToList();
+            nonMobileRoutes.ForEach(x => { swaggerDoc.Paths.Remove(x.Key); });
+        }
+    }
+}
